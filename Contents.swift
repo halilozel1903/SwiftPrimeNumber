89a0772bf115c50fd440daae79d26ce7c296d8f9
Halil Özel <halilozel1903@gmail.com>
2023-04-23 18:58:28 +0300

    Prime Number finds program added.

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,25 @@
-import UIKit
+func isPrime(_ number: Int) -> Bool {
+    if number < 2 {
+        return false
+    }
+    for i in 2..<number {
+        if number % i == 0 {
+            return false
+        }
+    }
+    return true
+}
 
-var greeting = "Hello, playground"
+func findPrimes(upTo limit: Int) -> [Int] {
+    var primes = [Int]()
+    for number in 2...limit {
+        if isPrime(number) {
+            primes.append(number)
+        }
+    }
+    return primes
+}
+
+let limit = 100
+let primes = findPrimes(upTo: limit)
+print("Prime numbers up to \(limit): \(primes)")
